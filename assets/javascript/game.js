aef5999c6b8b03c41fc9323e889fe82952f57fc8
bparvin12 <bparvin12@gmail.com>
2018-11-20 15:51:49 -0800

    finished unit 4 game

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -8,6 +8,7 @@ var numButton2 = getRandomInt(1, 12);
 var numButton3 = getRandomInt(1, 12);
 var numButton4 = getRandomInt(1, 12);
 var currentScore = 0;
+// var winLoss = "";
 
 
 
@@ -17,14 +18,14 @@ function getRandomInt(min, max) {
     max = Math.floor(max);
     return Math.floor(Math.random() * (max - min + 1)) + min;
 }
-function currentScoreAdder(but1, but2, but3, but4) {
-    numButt;
-}
+
 
 // gets html ready before js
 $(document).ready(function () {
 
     startGame = function () {
+
+
         // create random number gen from 19-120
         //get random number you have to add up to 
         $(".randomNum").text(randomNum);
@@ -34,45 +35,68 @@ $(document).ready(function () {
         $(".image1").on("click", function () {
             currentScore += numButton1;
             $(".yourScore").text(currentScore);
-
+            checkIfWon();
         });
 
         $(".image2").on("click", function () {
             currentScore += numButton2;
             $(".yourScore").text(currentScore);
-
+            checkIfWon();
         });
 
         $(".image3").on("click", function () {
             currentScore += numButton3;
             $(".yourScore").text(currentScore);
+            checkIfWon();
 
         });
         $(".image4").on("click", function () {
             currentScore += numButton4;
             $(".yourScore").text(currentScore);
+            checkIfWon();
 
         });
 
     };
-    // not sure if i put this if else statement in the startgame funciton
-        if (currentScore == randomNum) {
+
+    checkIfWon = function () {
+
+        if (currentScore > randomNum) {
+            lossCount++;
+            $(".lossCount").text(lossCount);
+            // $(".winLoss").text("You Loss");
+
+
+            startingScore = 0
+            randomNum = getRandomInt(19, 120);
+            $(".randomNum").text(randomNum);
+            numButton1 = getRandomInt(1, 12);
+            numButton2 = getRandomInt(1, 12);
+            numButton3 = getRandomInt(1, 12);
+            numButton4 = getRandomInt(1, 12);
+            currentScore = 0;
+
+        };
+
+        if (currentScore === randomNum) {
             winCount++;
             $(".winCount").text(winCount);
+            // $(".winLoss").text("You Loss");
+
+
+            startingScore = 0
+            randomNum = getRandomInt(19, 120);
+            $(".randomNum").text(randomNum);
+            numButton1 = getRandomInt(1, 12);
+            numButton2 = getRandomInt(1, 12);
+            numButton3 = getRandomInt(1, 12);
+            numButton4 = getRandomInt(1, 12);
+            currentScore = 0;
+
         };
 
 
-
-    
-
-
-
-
-
-
-
-
-
+    };
 
     startGame();
 
